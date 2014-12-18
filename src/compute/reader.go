9ddc1caf57f7ab3630a3976e9458eb6c66734b4f
Anumodh Narayanan <anumodh@gmail.com>
2014-12-19 02:13:37 +0530

    adding new regent

--- src/compute/reader.go
+++ src/compute/reader.go
@@ -7,10 +7,7 @@ import (
 	"fmt"
 	"github.com/anaray/regnet"
 	"net/http"
-	"regexp"
 	"time"
-	"strings"
-	"sync"
 )
 
 type LogReaderCompute struct {
@@ -28,22 +25,18 @@ func LogReader() *LogReaderCompute {
 	r.AddPattern("LOGLEVEL", `([Aa]lert|ALERT|[Tt]race|TRACE|[Dd]ebug|DEBUG|[Nn]otice|NOTICE|[Ii]nfo|INFO|[Ww]arn?(?:ing)?|WARN?(?:ING)?|[Ee]rr?(?:or)?|ERR?(?:OR)?|[Cc]rit?(?:ical)?|CRIT?(?:ICAL)?|[Ff]atal|FATAL|[Ss]evere|SEVERE|EMERG(?:ENCY)?|[Ee]merg(?:ency)?)`)
 	r.AddPattern("MS_DELIM", `%{MS_DATE_TIME}\s%{MS_TZ}\s%{LOGLEVEL}`)
 	return &LogReaderCompute{Regnet: r}
-	//return &LogReaderCompute{}
 }
 
 func (reader *LogReaderCompute) Execute(arg Args) {
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		filePath := r.FormValue("path")
-		//start
-		rgexp, _ := regexp.Compile(`((\d\d[\/]*){3}[\s]*(\d\d[:]*){3}.(\d*)) ((?:[PMCEI][SD]T|UTC)|GMT-\d\d:\d\d) ([Aa]lert|ALERT|[Tt]race|TRACE|[Dd]ebug|DEBUG|[Nn]otice|NOTICE|[Ii]nfo|INFO|[Ww]arn?(?:ing)?|WARN?(?:ING)?|[Ee]rr?(?:or)?|ERR?(?:OR)?|[Cc]rit?(?:ical)?|CRIT?(?:ICAL)?|[Ff]atal|FATAL|[Ss]evere|SEVERE|EMERG(?:ENCY)?|[Ee]merg(?:ency)?)`)
-		//end
-		go reader.process(filePath, arg, rgexp)
+		go reader.process(filePath, arg)
 	}
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func (reader *LogReaderCompute) process(file string, arg Args, regexp *regexp.Regexp) {
+func (reader *LogReaderCompute) process(file string, arg Args) {//, regexp *regexp.Regexp) {
 	fmt.Println("parsing started at ::", time.Now())
 	//initialize http handler and listen for POST message, get file name from request
 
@@ -80,30 +73,20 @@ func (reader *LogReaderCompute) process(file string, arg Args, regexp *regexp.Re
 			break
 		}
 
-		if regexp.Match(line) == true {
+		exists, _ := reader.Regnet.Exists(line, "%{MS_DELIM}")
+		if exists {
 			if lg != nil && len(lg.Store) > 0 {
 				//push it further
 				packet := NewPacket()
 				packet["log"] = lg
 				packet["source"] = file
-				/*logEntry := packet["log"].(*Logs)
-				last := strings.TrimSpace(string(logEntry.Store[ (len(logEntry.Store) - 25) : len(logEntry.Store)]))
-		
-				fmt.Println("lastline<",last,">")
-				if strings.EqualFold(last,"ThreadPoolImpl.java:127)") == false {
-					fmt.Println("Jumbled:", string(logEntry.Store))
-					fmt.Println("===========================================================")
-					//fmt.Println("The String version:", logEntry.StoreInString)
-					os.Exit(1)
-				}*/
 				arg.Outgoing <- packet
 			}
 			lg = NewLog(line)
 		} else {
 			if len(lg.Store) > 0 {
-			
-			store := []byte(lg.Store)	
-			lg.Store = string(append(store[:], line[:]...))
+				store := []byte(lg.Store)	
+				lg.Store = string(append(store[:], line[:]...))
 			}
 		}
 	}
@@ -114,3 +97,4 @@ func NewLog(line []byte) *Logs {
 	log.Store = string(line)
 	return log
 }
+
